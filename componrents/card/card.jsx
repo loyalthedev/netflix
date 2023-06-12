9b54026838467d139a5ae7ae0554ebdbfb01ff2a
daddyloyal <lifeofemmany6@gmail.com>
2023-06-12 12:41:56 +0100

    Scaling cards with framer

---
+++ componrents/card/card.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Image from "next/image";
+
+import cls from "classnames";
+import { motion } from "framer-motion";
+
+import styles from "./card.module.css";
+
+const Card = (props) => {
+  const { imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1159&q=80", size='medium', id } = props;
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+  const handleOnError = () => {
+    console.log("hii error");
+    setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1159&q=80");
+  };
+  const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
+
+  return (
+    // <div className={styles.container}>
+    //   <motion.div
+    //     className={cls(styles.imgMotionWrapper, classMap[size])}
+    //     whileHover={{ ...scale }}
+    //   >
+    //     <Image
+    //       src={imgSrc}
+    //       alt="image"
+    //       layout="fill"
+    //       onError={handleOnError}
+    //       className={styles.cardImg}
+    //     />
+    //   </motion.div>
+    // </div>
+
+    <div className={styles.container}>
+      <motion.div
+        className={cls(styles.imgMotionWrapper, classMap[size])}
+        whileHover={{ ...scale }}
+      >
+        <Image
+          src={imgSrc}
+          alt="image"
+          layout="fill"
+          onError={handleOnError}
+          className={styles.cardImg}
+        />
+      </motion.div>
+    </div>
+  );
+};
+
+export default Card;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ["images.unsplash.com", "i.ytimg.com"],
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

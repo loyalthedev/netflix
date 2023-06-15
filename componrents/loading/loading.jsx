c1748e41598291c5c3c769c80a59f4fe50a39918
daddyloyal <lifeofemmany6@gmail.com>
2023-06-15 21:17:57 +0100

    Made some changes

--- componrents/loading/loading.jsx
+++ componrents/loading/loading.jsx
@@ -2,9 +2,7 @@ import styles from "./loading.module.css";
 
 const Loading = () => {
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <p className={styles.loader}>Loading...</p>
-    </div>
   );
 };
 
